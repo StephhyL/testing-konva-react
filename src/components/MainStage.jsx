ad17e4473afa818ef46447d2be0af4de9e178da2
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-14 00:52:37 -0500

    style

--- src/components/MainStage.jsx
+++ src/components/MainStage.jsx
@@ -33,46 +33,55 @@ const MainStage = () => {
   return (
     <div className="creativity">
       <div className="sidebar">
-        <h5>Select Shape Fill Color</h5>
-        <CirclePicker
-          color={fillColor}
-          onChange={(fillColor) => setFillColor(fillColor.hex)}
-        />
-        <h5>Select Stroke Color</h5>
-        <CirclePicker
-          color={strokeColor}
-          onChange={(strokeColor) => setStrokeColor(strokeColor.hex)}
-        />
-        <h3>Select a Shape</h3>
-        <Button
-          variant="outline-secondary"
-          onClick={() => handleClick("rectangle", fillColor, strokeColor)}
-        >
-          Rectangle
-        </Button>
-        <Button
-          variant="outline-secondary"
-          onClick={() => handleClick("star", fillColor, strokeColor)}
-        >
-          Star
-        </Button>
-        <form
-          autoComplete="off"
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <input
-            name="url"
-            type="text"
-            placeholder="Enter URL of Image"
-            value={url}
-            onChange={(e) => setURL(e.target.value)}
+        <div className="select-a-color">
+          <h5>Select Shape Fill Color</h5>
+          <CirclePicker
+            color={fillColor}
+            onChange={(fillColor) => setFillColor(fillColor.hex)}
           />
-        </form>
-        <Button confirm="confirm" onClick={() => handleClick("", "", "", url)}>
-          Add!!
-        </Button>
+          <h5>Select Stroke Color</h5>
+          <CirclePicker
+            color={strokeColor}
+            onChange={(strokeColor) => setStrokeColor(strokeColor.hex)}
+          />
+        </div>
+        <div className="shapes">
+          <h3>Select a Shape</h3>
+          <Button
+            variant="outline-secondary"
+            onClick={() => handleClick("rectangle", fillColor, strokeColor)}
+          >
+            Rectangle
+          </Button>
+          <Button
+            variant="outline-secondary"
+            onClick={() => handleClick("star", fillColor, strokeColor)}
+          >
+            Star
+          </Button>
+        </div>
+        <div className="add-url">
+          <form
+            autoComplete="off"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              name="url"
+              type="text"
+              placeholder="Enter URL of Image"
+              value={url}
+              onChange={(e) => setURL(e.target.value)}
+            />
+          </form>
+          <Button
+            variant="outline-secondary"
+            onClick={() => handleClick("", "", "", url)}
+          >
+            Add!!
+          </Button>
+        </div>
       </div>
       <div className="stage">
         <Stage
